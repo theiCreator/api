fcdcc14f04cd5598b194a958e58e5f71d8ade37c
theicreator <saintkhizzy0@gmail.com>
2024-02-10 23:47:23 +0100

    learnable task

---
+++ card verification/card.js
@@ -0,0 +1,22 @@
+function validateCreditCard(cardNumber) {
+  // Regex patterns for Visa,Verve and Mastercard cards
+  const visa = /^4[0-9]{12}(?:[0-9]{3})?$/;
+  const mastercard = /^5[1-5][0-9]{14}$/;
+  const verve = /^(506[0-1][0-8][0-9]|6500(0[2-9]|1[0-9]|2[0-7])|507(8[6-9][0-9]|9[0-6][0-4]))[0-9]{10,13}$/;
+
+
+  // Validation process
+    if (visa.test(cardNumber)) {
+      return "A Visa Card";
+  } else if (mastercard.test(cardNumber)) {
+      return "A Mastercard";
+  } else if (verve.test(cardNumber)) {
+      return "A Verve Card";
+  } else{
+      return "Not Found";
+  }
+}
+// Usage:
+const cardNumber = "5061081106797727793"; // Replace with card number for validation
+const typeOfCard = validateCreditCard(cardNumber);
+console.log(`This card is: ${typeOfCard}`);
