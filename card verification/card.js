64b0c859a01a1f7b95e27a65b9cf30d8be12d4d5
theicreator <saintkhizzy0@gmail.com>
2024-02-11 00:23:52 +0100

    changes

--- card verification/card.js
+++ card verification/card.js
@@ -20,3 +20,18 @@ function validateCreditCard(cardNumber) {
 const cardNumber = "0123456789"; // Replace with card number for validation
 const typeOfCard = validateCreditCard(cardNumber);
 console.log(`This card is: ${typeOfCard}`);
+
+// Descriptions of patterns used
+/*
+ Visa:
+    Begins with 4.
+    Followed by 12 digits.
+    Optionally followed by 3 more digits.
+ Mastercard:
+    Begins with 5.
+    The second digit is any of the numbers 1 to 5.
+    Followed by 14 digits.
+ Verve:
+    Begins with either 506099-506198 or 650002-650027 or 507865-507964
+    Followed by 10,11,12 or 13 digits, since verve cards can have 16-19 digits.
+*/
